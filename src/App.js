cf8dfc63c611101bc647ed6ae1884894d622b023
Yaser Aly <77906579+yasermaly@users.noreply.github.com>
2022-08-01 22:09:44 -0700

    wip-button deletes names

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import Final from './tutorial/11-react-router/final/index'
-import Setup from "./tutorial/1-useState/setup/1-error-example";
+import Setup from "./tutorial/1-useState/setup/3-useState-array";
 
 function App() {
   return (
     <div className='container'>
       <Setup />
-      <Final />
     </div>
   )
 }
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+
+ const [text, setText] = useState('random title');
+
+ const handleClick = () => {
+  if(text === 'random title') {
+    setText('hello world');
+  } else { 
+    setText('random title');
+  }
+}
+
+ return (
+  <React.Fragment>
+    <h1>{text}</h1>
+    <button onClick={handleClick} className="btn">
+    change title
+    </button>
+  </React.Fragment> )
+
 };
+
 
 export default UseStateBasics;
